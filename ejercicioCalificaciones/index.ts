eb33834abd2faf8fe3f8013039e78d51d36fc986
Arturo-Blanco <103155759+Arturo-Blanco@users.noreply.github.com>
2022-05-16 15:51:26 -0300

    Se agrega condicion de ingreso si hay alumno para calificar

--- ejercicioCalificaciones/index.ts
+++ ejercicioCalificaciones/index.ts
@@ -1,11 +1,12 @@
 let alumno: string = String(prompt("Ingrese nombre alumno"));
-let nota1: number = Number(prompt("Ingrese nota practica"));
-let nota2: number = Number(prompt("Ingrese nota problemas"));
-let nota3: number = Number(prompt("Ingrese nota teorica"));
-let condicion = nota1 <= 10 && nota2 <= 10 && nota3 <= 10;
 let notaTotal: number = 0;
 
 while (alumno !== "") {
+  let nota1: number = Number(prompt("Ingrese nota practica"));
+  let nota2: number = Number(prompt("Ingrese nota problemas"));
+  let nota3: number = Number(prompt("Ingrese nota teorica"));
+  let condicion = nota1 <= 10 && nota2 <= 10 && nota3 <= 10;
+
   if (condicion === true) {
     notaTotal = Number(nota1 * 0.1 + nota2 * 0.5 + nota3 * 0.4);
     console.log("La nota final de: " + alumno + " es " + notaTotal);
