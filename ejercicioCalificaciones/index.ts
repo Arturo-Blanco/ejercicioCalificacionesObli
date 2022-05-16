d45f173b0acf3338967529267fc449cff4234115
Arturo-Blanco <103155759+Arturo-Blanco@users.noreply.github.com>
2022-05-16 15:42:42 -0300

    Se modifica posicion if y while

--- ejercicioCalificaciones/index.ts
+++ ejercicioCalificaciones/index.ts
@@ -2,15 +2,16 @@ let alumno: string = String(prompt("Ingrese nombre alumno"));
 let nota1: number = Number(prompt("Ingrese nota practica"));
 let nota2: number = Number(prompt("Ingrese nota problemas"));
 let nota3: number = Number(prompt("Ingrese nota teorica"));
-
+let condicion = nota1 <= 10 && nota2 <= 10 && nota3 <= 10;
 let notaTotal: number = 0;
 
-if (alumno !== "") {
-  while (nota1 <= 10 && nota2 <= 10 && nota3 <= 10) {
+while (alumno !== "") {
+  if (condicion === true) {
     notaTotal = Number(nota1 * 0.1 + nota2 * 0.5 + nota3 * 0.4);
     console.log("La nota final de: " + alumno + " es " + notaTotal);
     break;
+  } else {
+    console.log("Error en la carga de datos");
+    break;
   }
-} else {
-  console.log("Error en la carga de datos");
 }
